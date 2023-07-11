aecf7774d96b3b451fc1bf589e18f3d96d32a785
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-10 21:05:06 -0300

    created table "orgaos"

---
+++ database/migrations/2023_07_10_000000_create_orgaos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    private const TABLE = 'orgaos';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->id();
+            $table->integer('seq_orgao');
+            $table->integer('num_anoexercicio');
+            $table->text('cod_orgao');
+            $table->text('nom_orgao');
+            $table->text('tipo_orgao');
+            $table->integer('cod_municipio');
+            $table->text('nom_municipio');
+            $table->text('cod_uf');
+            $table->text('sgl_uf');
+            $table->text('nom_uf');
+            $table->text('dsc_regiaoplanejamento');
+            $table->text('num_versao_arq');
+
+            $table->timestamps();
+        });
+    }
+//
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
