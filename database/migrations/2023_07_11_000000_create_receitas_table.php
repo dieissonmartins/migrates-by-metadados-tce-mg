7c4b44d6ad29cc51f1f6f7ecdd545f98ff7b7247
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-11 13:21:07 -0300

    created table "receitas"

---
+++ database/migrations/2023_07_11_000000_create_receitas_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Informações referentes as Receitas dos Órgãos Municipais.
+     */
+    protected const TABLE = 'receitas';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('seq_orgao');
+            $table->integer('num_anoexercicio');
+            $table->text('dsc_cateconomica');
+            $table->text('num_mesexercicio');
+            $table->text('dsc_origem');
+            $table->text('dsc_especie');
+            $table->text('dsc_rubrica');
+            $table->text('dsc_alinea');
+            $table->text('dsc_subalinea');
+            $table->text('dsc_tipo');
+            $table->text('dsc_fonterecurso');
+            $table->integer('vlr_previsaoinicial');
+            $table->integer('vlr_previsaoatualizada');
+            $table->integer('vlr_realizadoateperiodo');
+            $table->text('num_versaoarq');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
